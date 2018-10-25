ecc48044ce375694afeae89fe0efde71d921ac47
Jan Weidner <jw3126@gmail.com>
2018-10-25 05:01:50 -0700

    fix

--- src/testing.jl
+++ src/testing.jl
@@ -1,6 +1,6 @@
 module Testing
 using SimpleSparseArrays
-export isapprox_full
+export isapprox_full, memory_ratio, storage_ratio
 
 function isapprox_full(f, args_sparse...; kw...)
     args_full = map(args_sparse) do arg
@@ -13,6 +13,16 @@ function isapprox_full(f, args_sparse...; kw...)
     ret_full = f(args_full...)
     ret_sparse = f(args_sparse...)
     isapprox(ret_full, ret_sparse; kw...)
+end
+
+function memory_ratio(arr::SimpleSparseArray)
+    @assert isbitstype(eltype(arr))
+    mem_full = length(arr) * sizeof(eltype(arr))
+    Base.summarysize(arr) / mem_full
+end
+
+function storage_ratio(arr::SimpleSparseArray)
+    length(eachstoredindex(arr)) / length(arr)
 end
 
 end#module
